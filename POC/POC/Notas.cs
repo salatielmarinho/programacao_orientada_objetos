1415ad4d78cd4cefce7c67e63829ea6747e3238a
salatielmarinho <salatielmarinho@gmail.com>
2024-03-23 21:55:09 -0300

    Subida Inicial

---
+++ POC/POC/Notas.cs
@@ -0,0 +1,26 @@
+﻿namespace POC
+{
+    public class Notas
+    {
+        public int Nota1 { get; set; }
+        public int Nota2 { get; set; }
+        public int Nota3 { get; set; }
+        public int Nota4 { get; set; }
+
+        public int CalcularMediaAnual(Notas notas)
+        {
+            int mediaAnual = (notas.Nota1 + notas.Nota2 + notas.Nota3 + notas.Nota4) / 4;
+            return mediaAnual;
+        }
+
+        public bool AplicarRegraMediaAnual(int mediaAnual)
+        {
+            bool retornoCalcularMediaAnual = false;
+            if (mediaAnual >= 7)
+            {
+                retornoCalcularMediaAnual = true;
+            }
+            return retornoCalcularMediaAnual;
+        }
+    }
+}
---
+++ POC/POC/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace POC
+{
+    internal class Program
+    {
+        //Passo 1 - Declarar Variaveis        
+        static int retornoCalcularMediaAnual;
+        static bool retornoRegraMediaAnual;
+
+        static void Main(string[] args)
+        {
+            //Passo 2 - Realizar input de notas anuais
+            Notas notas = new Notas();
+            notas = CapturarNotas();
+
+            //Passo 3 - Realizar Cálculo de Média Anual na classe Notas
+            retornoCalcularMediaAnual = notas.CalcularMediaAnual(notas);
+
+            //Passo 4 - Realizar Validação da Média Anual na classe Notas
+            retornoRegraMediaAnual = notas.AplicarRegraMediaAnual(retornoCalcularMediaAnual);
+
+            //Passo 5 - Apresentar Mensagem de Media Anual (aprovado/reprovado)
+            ApresentarMensagemMediaAnual(retornoRegraMediaAnual);
+        }
+
+        private static Notas CapturarNotas()
+        {
+            Notas notas = new Notas();
+
+            //Label
+            Console.WriteLine("Informar a nota1: ");
+            //Entrada realizada pelo usuário
+            //Toda entrada realizada pelo usuário é STRING
+            //Converter o dado que existe dentro do Console.ReadLine que é string para o tipo da 
+            //variavel definida (ex: string --> int)
+            notas.Nota1 = Convert.ToInt16(Console.ReadLine());
+
+            //Label
+            Console.WriteLine("Informar a nota2: ");
+            //Entrada realizada pelo usuário
+            //Toda entrada realizada pelo usuário é STRING
+            //Converter o dado que existe dentro do Console.ReadLine que é string para o tipo da 
+            //variavel definida (ex: string --> int)
+            notas.Nota2 = Convert.ToInt16(Console.ReadLine());
+
+            //Label
+            Console.WriteLine("Informar a nota3: ");
+            //Entrada realizada pelo usuário
+            //Toda entrada realizada pelo usuário é STRING
+            //Converter o dado que existe dentro do Console.ReadLine que é string para o tipo da 
+            //variavel definida (ex: string --> int)
+            notas.Nota3 = Convert.ToInt16(Console.ReadLine());
+
+            //Label
+            Console.WriteLine("Informar a nota4: ");
+            //Entrada realizada pelo usuário
+            //Toda entrada realizada pelo usuário é STRING
+            //Converter o dado que existe dentro do Console.ReadLine que é string para o tipo da 
+            //variavel definida (ex: string --> int)
+            notas.Nota4 = Convert.ToInt16(Console.ReadLine());
+
+            return notas;
+        }
+
+        private static void ApresentarMensagemMediaAnual(bool retornoRegraMediaAnual)
+        {
+            if (retornoRegraMediaAnual)
+            {
+                Console.WriteLine(String.Format("Aluno Aprovado: {0}", retornoCalcularMediaAnual));
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Aluno reprovado " + retornoCalcularMediaAnual);
+                Console.ReadKey();
+            }
+        }
+    }
+}
